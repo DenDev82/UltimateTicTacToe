f1835bf8554e50a60900c44068374f5fad1108b5
DenDev82 <dendev82@gmail.com>
2026-04-19 15:21:59 +0200

    mjau

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:3000");
+const socket = io();
 const gameContainer = document.getElementById("game");
 const messageDisplay = document.createElement("div");
 messageDisplay.id = "message-display";
